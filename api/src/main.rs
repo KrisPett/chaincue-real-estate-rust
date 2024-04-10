986cbcb84703b065b8b24596af2171544ee490ae
KQT3 <70701718+KQT3@users.noreply.github.com>
2024-04-10 22:03:52 +0200

    commit

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{App, HttpServer, middleware};
+use actix_web::{web, App, HttpServer, middleware};
+use sea_orm::DatabaseConnection;
 
 use configs::connect_db;
 use routes::home_page;
@@ -7,14 +8,22 @@ use routes::houses_page;
 mod routes;
 mod configs;
 
+#[derive(Debug, Clone)]
+struct AppState {
+    conn: DatabaseConnection,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    connect_db::connect_postgres().await?;
+    let conn = connect_db::connect_postgres().await.unwrap();
 
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
+    let state = AppState { conn };
+
     HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(state.clone()))
             .wrap(middleware::Logger::default())
             .configure(houses_page::init_routes)
             .configure(home_page::init_routes)

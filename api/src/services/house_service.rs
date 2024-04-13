234847c53a1383944c15c3eed563ad0b216136f4
KQT3 <70701718+KQT3@users.noreply.github.com>
2024-04-13 11:42:14 +0200

    commit

--- api/src/services/house_service.rs
+++ api/src/services/house_service.rs
@@ -1,12 +1,11 @@
-use actix_web::{Error, HttpResponse, Responder, web};
-use actix_web::error::InternalError;
-use actix_web::http::StatusCode;
 use chrono::{DateTime, Utc};
 use sea_orm::{ActiveModelTrait, DbErr, Set};
 use sea_orm::prelude::DateTimeWithTimeZone;
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
-
+use actix_web::{
+    error, get, middleware, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Result, error::InternalError
+};
+use actix_web::http::StatusCode;
 use entity::countries;
 use entity::countries::Model;
 use entity::sea_orm_active_enums::CountryName;
@@ -21,40 +20,11 @@ struct CountryResponse {
     country_name: CountryName,
 }
 
-// pub async fn insert(data: web::Data<AppState>) -> impl Responder {
-//     let dbc = &data.dbc;
-//     let country = countries::ActiveModel {
-//         id: Set(String::from(Uuid::new_v4())),
-//         created_at: Set(DateTimeWithTimeZone::from(chrono::Utc::now())),
-//         updated_at: Set(DateTimeWithTimeZone::from(chrono::Utc::now())),
-//         country_name: Set(Some(CountryName::Spain)),
-//     };
-//
-//     country
-//
-//     // match country.insert(dbc).await {
-//     //     Ok(model) => {
-//     //         let response = CountryResponse {
-//     //             id: model.id.unwrap_or_default(),
-//     //             created_at: model.created_at.unwrap_or(Utc::now()),
-//     //             updated_at: model.updated_at.unwrap_or(Utc::now()),
-//     //             country_name: CountryName::Spain
-//     //
-//     //         };
-//     //         HttpResponse::Ok().json(response)
-//     //     },
-//     //     Err(e) => {
-//     //         eprintln!("Failed to insert country: {:?}", e);
-//     //         HttpResponse::InternalServerError().json("Failed to insert country")
-//     //     }
-//     // }
-//
-// }
-
-pub async fn insert(data: web::Data<AppState>) -> Result<Model, CustomError> {
+pub async fn insert(data: web::Data<AppState>) -> Result<Model, Error> {
     let dbc = &data.dbc;
     let country = countries::ActiveModel {
-        id: Set(String::from(Uuid::new_v4())),
+        // id: Set(String::from(Uuid::new_v4())),
+        id: Set("5447aa63-58cc-4d83-ad1d-168e6a6095b3".parse()?),
         created_at: Set(DateTimeWithTimeZone::from(chrono::Utc::now())),
         updated_at: Set(DateTimeWithTimeZone::from(chrono::Utc::now())),
         country_name: Set(Some(CountryName::Spain)),
@@ -62,6 +32,7 @@ pub async fn insert(data: web::Data<AppState>) -> Result<Model, CustomError> {
 
     match country.insert(dbc).await {
         Ok(model) => Ok(model),
-        Err(err) => Err(CustomError::InsertCountryFailed),
+        // Err(err) => Err(InternalError::new(err, StatusCode::INTERNAL_SERVER_ERROR).into()),
+        Err(err) => Err(Error::from(CustomError::DatabaseError(err))),
     }
 }

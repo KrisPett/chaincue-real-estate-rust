528726a5290e53daca671369a9a9f3ef75610ff9
KQT3 <70701718+KQT3@users.noreply.github.com>
2024-04-15 20:12:21 +0200

    commit

--- api/src/routes/home_page/home_page.rs
+++ api/src/routes/home_page/home_page.rs
@@ -1,7 +1,85 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{Error, get, HttpResponse, web};
+use sea_orm::{ActiveEnum, DatabaseConnection, EntityTrait};
+use serde::{Deserialize, Serialize};
+
+use crate::AppState;
+use crate::middlewares::errors::CustomErrors;
+
+#[derive(Serialize, Deserialize)]
+struct HomePageDTO {
+    countries: Vec<CountryDTO>,
+    houses: Vec<HouseDTO>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct CountryDTO {
+    name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct HouseDTO {
+    id: String,
+    title: String,
+    location: String,
+    r#type: String,
+    number_rooms: i64,
+    beds: i64,
+    dollar_price: String,
+    crypto_price: String,
+    src: String,
+}
+
+struct DTOBuilder {
+    countries: Vec<entity::countries::Model>,
+    houses: Vec<entity::houses::Model>,
+}
 
 #[get("/home")]
-pub async fn get_hey() -> impl Responder {
+pub async fn get_hey(data: web::Data<AppState>) -> Result<HttpResponse, Error> {
+    let dto = build_dto(&data.dbc).await?;
+    Ok(HttpResponse::Ok().json(dto))
+}
 
-    HttpResponse::Ok().body("home!")
+async fn build_dto(dbc: &DatabaseConnection) -> Result<HomePageDTO, Error> {
+    let mut dto_builder = DTOBuilder {
+        countries: Vec::new(),
+        houses: Vec::new(),
+    };
+    // let countries = entity::countries::Entity::find().all(dbc).await
+    //     .map_err(|err| Error::from(CustomErrors::DatabaseError(err)))?;
+
+    let result = match entity::countries::Entity::find().all(dbc).await {
+        Ok(model) => Ok(model),
+        Err(err) => Err(std::io::Error::from(CustomErrors::DatabaseError(err)))
+    }?;
+    dto_builder.countries = result;
+
+    Ok(to_home_page_dto(dto_builder))
+}
+
+fn to_home_page_dto(dto_builder: DTOBuilder) -> HomePageDTO {
+    HomePageDTO {
+        countries: dto_builder.countries.into_iter().map(to_country_dto).collect(),
+        houses: dto_builder.houses.into_iter().map(to_house_dto).collect(),
+    }
+}
+
+fn to_country_dto(country: entity::countries::Model) -> CountryDTO {
+    CountryDTO {
+        name: country.country_name.to_value()
+    }
+}
+
+fn to_house_dto(house: entity::houses::Model) -> HouseDTO {
+    HouseDTO {
+        id: house.id,
+        title: house.title.unwrap_or(String::new()),
+        location: house.location.unwrap_or(String::new()),
+        r#type: house.house_types.to_value(),
+        number_rooms: house.number_rooms.unwrap_or(0),
+        beds: house.beds.unwrap_or(0),
+        dollar_price: "".to_string(),
+        crypto_price: String::from("₿32.346"),
+        src: house.src.unwrap_or(String::new()),
+    }
 }
